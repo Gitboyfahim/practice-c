f2bdc2181a5fd7e5669617c385683937208858f4
Gitboyfahim <fshariarabcd@gmail.com>
2024-09-20 23:51:13 +0600

    before mid c code

---
+++ beforemid/if10.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+int main()
+
+{
+	int monthnum;
+printf("Enter number of month[1-12]: \n");
+scanf("%d",&monthnum);
+
+if(monthnum==1)
+{
+printf("Days =31\n");
+}
+
+else if(monthnum==2)
+{
+printf("Days =28 or  29[If Leapyear]\n");
+}
+
+else if(monthnum==3)
+{
+printf("Days =31\n");
+}
+
+else if(monthnum==4)
+{
+printf("Days =30\n");
+}
+
+else if(monthnum==5)
+{
+printf("Days =31\n");
+}
+
+else if(monthnum==6)
+{
+printf("Days =30\n");
+}
+
+else if(monthnum==7)
+{
+printf("Days =31\n");
+}
+
+else if(monthnum==8)
+{
+printf("Days =31\n");
+}
+
+else if(monthnum==9)
+{
+printf("Days =30\n");
+}
+
+else if(monthnum==10)
+{
+printf("Days =31\n");
+}
+
+else if(monthnum=11)
+{
+printf("Days =30\n");
+}
+
+else if(monthnum=12)
+{
+printf("Days =31\n");
+}
+
+else
+{
+printf("Month number is invalid\n");
+}
+
+	return 0;
+}
