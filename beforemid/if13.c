5e713bfbd44ac9a6cdc0aa75537383cf869a5fa8
Gitboyfahim <fshariarabcd@gmail.com>
2024-09-21 12:21:10 +0600

    before mid c code

---
+++ beforemid/if13.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+
+int s1,s2,s3;
+	printf("Enter three sides of triangle: \n");
+	scanf("%d %d %d",&s1,&s2,&s3);
+if((s1==s2) && (s2==s3))
+{
+	printf("Your triangle is equilateral\n");
+}
+else if(s1!=s2 && s2!=s3 && s3!=s1)
+{
+	printf("Your triangle is scalene\n");
+}
+else
+{
+	printf("Your triangle is isosceles\n");
+}
+	return 0;
+}
