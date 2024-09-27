d44dab1a56d05a63dd646bb0a2887a027690b6c8
Fahim Shariar <fshariarabcd@gmail.com>
2024-09-28 01:04:33 +0600

    triangle beforemid c code

--- beforemid/if12.c
+++ beforemid/if12.c
@@ -2,13 +2,13 @@
 int main()
 {
 	int s1,s2,s3;
-printf("Enter 3 sides of triangle: \n");
+ printf("Enter 3 sides of triangle: \n");
 scanf("%d %d %d",&s1,&s2,&s3);
  	if((s1+s2>3) && (s2+s3>s1) && (s1+s3>s2))
-printf("This triangle is valid\n");			
+ printf("This triangle is valid\n");			
 	else 
 {
-printf("This triangle is not valid\n");
+ printf("This triangle is not valid\n");
 }
 
 
