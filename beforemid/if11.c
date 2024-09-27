3df41aa019915991e69d3e512413d6c39dcb3934
Fahim Shariar <fshariarabcd@gmail.com>
2024-09-28 01:03:52 +0600

    triangle beforemid c code

--- beforemid/if11.c
+++ beforemid/if11.c
@@ -5,12 +5,12 @@ int main()
 int ang1,ang2,ang3;
 printf("Enter angle1 angle2 and angle3: \n");
 scanf("%d %d %d",&ang1,&ang2,&ang3);
-if((ang1+ang2+ang3==180) && ang1!=0 &&ang2!=0 && ang3 != 0)
+	if((ang1+ang2+ang3==180) && ang1!=0 &&ang2!=0 && ang3 != 0)
 {
 printf("Triangle is valid\n");
 }
 
-else
+	else
 {
 printf("Triangle is not valid\n");
 }
