bed65a75940480e31ae200739a6f6235554116c5
Gitboyfahim <fshariarabcd@gmail.com>
2024-09-27 22:26:43 +0600

    beforemid c code notescalculation

---
+++ beforemid/if18.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+int ope1st,ope2nd;
+printf("Enter operator: \n");
+scanf("%c",&ch);
+printf("Enter 1st operand: ");
+scanf("%d",&ope1st);
+printf("Enter 2nd operand: ");
+scanf("%d",&ope2nd);
+
+switch(ch)
+{
+	case '+' :
+		printf("Result is:%d\n", (ope1st+ope2nd));
+		break;
+	case '-' :
+                printf("Result is:%d\n", (ope1st-ope2nd));
+                break;
+	case '*' :
+                printf("Result is:%d\n", (ope1st*ope2nd));
+                break;
+	case '/' :
+                printf("Result is:%d\n", (ope1st/ope2nd));
+                break;
+
+	default:
+		printf("Enter two valid numbers\n");
+		break;
+}
+
+
+
+
+	return 0;
+}
---
+++ beforemid/if19.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	char ch;
+printf("Enter character: \n");
+scanf("%c",&ch);
+if((ch>='0' && ch<='9') || (ch>='a' && ch<='f') || (ch>='A' && ch<='F'))
+		{
+printf("%c is a hex digit\n",ch);
+		}
+else 
+{
+printf("Not a hex digit\n");
+}
+		
+  
+
+
+
+	return 0;
+}
