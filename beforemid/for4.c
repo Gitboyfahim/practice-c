ec740c2ef3d2533ef5f5311a53ab21503ae26eaf
Fahim Shariar <fshariarabcd@gmail.com>
2024-10-04 00:53:31 +0600

    before mid for loop codes

---
+++ beforemid/for4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+
+	int i,n,sum=0;
+	printf("Enter the value of N: ");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		sum=(sum+i);
+	}
+	printf("sum= %d\n",sum);
+
+	return 0;
+}
